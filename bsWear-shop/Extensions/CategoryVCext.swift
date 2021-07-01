7a66883608f4aafeb6f68ed32b9a471059921de4
djud17 <davidtonoyan10@rambler.ru>
2021-07-02 02:12:22 +0300

    Complete parsing JSON

---
+++ bsWear-shop/Extensions/CategoryVCext.swift
@@ -0,0 +1,8 @@
+//
+//  CategoryVCext.swift
+//  bsWear-shop
+//
+//  Created by Давид Тоноян  on 01.07.2021.
+//
+
+import Foundation
---
+++ bsWear-shop/Model/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  bsWear-shop
+//
+//  Created by Давид Тоноян  on 01.07.2021.
+//
+
+import Foundation
